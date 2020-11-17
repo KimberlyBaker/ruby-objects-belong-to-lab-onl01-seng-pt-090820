bd25d2866c6bf388fe27f6d30928ec1906e536e5
Kimberly Baker <bakerk2010@gmail.com>
2020-11-17 19:29:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,14 +2,4 @@ class Artist
   
   attr_accessor :name
   
-  def initialize(name)
-    @name = name
-  end
-  
 end
-
-beyonce = Artist.new("Beyonce")
-
-# 711 = Song.new("7/11")
-
-# 711.artist = beyonce
